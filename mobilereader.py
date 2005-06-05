1e5e977a7e4e5a1acfeeb4fd9a9a293ee0891b10
takano32 <takano32@20b47ea9-8295-4682-bdc9-7f42c2971ab2>
2005-06-05 17:14:58 +0000

    open file
    git-svn-id: file:///home/takano32/.svnrepos/python/trunk@8 20b47ea9-8295-4682-bdc9-7f42c2971ab2

--- mobilereader.py
+++ mobilereader.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
 # vim: fileencoding=euc-jp
 # coding: euc-jp
+import os
 import nkf
 print u"普通の日本語"
 print unicode("日本語の量が", "japanese.euc_jp")
 print unicode("少ないとご認識", "japanese.euc_jp")
 
+
+for (root, dirs, files) in os.walk(u"."):
+  print root, dirs
+  #print dirs
+  for f in files:
+    if f == u'ふが':
+      print f
+      path = os.path.join(root, f)
+      infile = open(path, 'r')
+      data = infile.read()
+      print data
 
