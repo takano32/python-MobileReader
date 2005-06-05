b87b9ac69dd291bd7f4483ae4366f5b84ecf34c1
takano32 <takano32@20b47ea9-8295-4682-bdc9-7f42c2971ab2>
2005-06-05 14:21:53 +0000

    unicode
    git-svn-id: file:///home/takano32/.svnrepos/python/trunk@7 20b47ea9-8295-4682-bdc9-7f42c2971ab2

--- mobilereader.py
+++ mobilereader.py
@@ -2,8 +2,8 @@
 # vim: fileencoding=euc-jp
 # coding: euc-jp
 import nkf
-print "普通の日本語"
-print unicode(r"日本語の量が", "japanese.euc_jp")
-print unicode(r"少ないとご認識", "japanese.euc_jp")
+print u"普通の日本語"
+print unicode("日本語の量が", "japanese.euc_jp")
+print unicode("少ないとご認識", "japanese.euc_jp")
 
 
