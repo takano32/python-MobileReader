f63af62f51e9bc72b8615707c7f4f76bcfb3907b
takano32 <takano32@20b47ea9-8295-4682-bdc9-7f42c2971ab2>
2005-06-05 11:28:33 +0000

    *** empty log message ***
    git-svn-id: file:///home/takano32/.svnrepos/python/trunk@4 20b47ea9-8295-4682-bdc9-7f42c2971ab2

--- mobilereader.py
+++ mobilereader.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
-
-print '日本語の量が'
-print '少ないとご認識'
+# coding: euc-jp
+import nkf
+print "普通の日本語"
+print unicode(r"日本語の量が", "japanese.euc_jp")
+print unicode(r"少ないとご認識", "japanese.euc_jp")
 
 
