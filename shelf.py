ebed7cf96f57eacc17c4df191e439dedc33ef10d
takano32 <takano32@20b47ea9-8295-4682-bdc9-7f42c2971ab2>
2005-06-09 14:01:07 +0000

    implement shelf
    git-svn-id: file:///home/takano32/.svnrepos/python/trunk@19 20b47ea9-8295-4682-bdc9-7f42c2971ab2

--- shelf.py
+++ shelf.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
-
+import os
+import fnmatch
+import book
 class Shelf:
+  def __init__(self, path, pattern):
+    self.path = os.path.abspath(path)
+    if not self.path.startswith(os.path.abspath(".")):
+      raise RuntimeError("Permission denied: " + abspath)
+    self.pattern = pattern
 
+  def list(self):
+    alist = list()
+    for(root, dirs, files) in os.walk(self.path):
+      for file in files:
+        if fnmatch.fnmatch(file, self.pattern):
+          alist.append(file)
+    return alist
 
-
-
+shelf = Shelf("./maria", "*.txt")
+for file in  shelf.list():
+  print file
+#for book in shelf.list():
+#  print book
