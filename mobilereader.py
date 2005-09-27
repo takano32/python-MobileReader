98fc0ca14f976c7579ff95250619ee9f45a55b7f
takano32 <takano32@20b47ea9-8295-4682-bdc9-7f42c2971ab2>
2005-09-27 06:32:23 +0000

    use cgi
    git-svn-id: file:///home/takano32/.svnrepos/python/trunk@27 20b47ea9-8295-4682-bdc9-7f42c2971ab2

--- mobilereader.py
+++ mobilereader.py
@@ -2,5 +2,15 @@
 # coding: euc-jp
 # vim: fileencoding=euc-jp
 
+import cgi
+
+form = cgi.FieldStorage()
+if form.has_key('inputname'):
+  # inputname が指定されていたら、ご挨拶を表示
+  inputname = form['inputname'].value
+  print _output_form % inputname
+else:
+  # 入力フォームを表示
+  print _input_form
 
 
